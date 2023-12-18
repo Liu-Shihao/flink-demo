a7ad09e125d8d9ad91f5a90373a559df6b7d898b
LiuShihao <liush99@foxmail.com>
2023-12-18 16:37:22 +0800

    first commit

---
+++ src/main/java/com/lsh/FlinkDemoApplication.java
@@ -0,0 +1,12 @@
+package com.lsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlinkDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkDemoApplication.class,args);
+    }
+}
---
+++ src/test/java/com/lsh/FlinkTest.java
@@ -0,0 +1,47 @@
+package com.lsh;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+import org.junit.jupiter.api.Test;
+
+public class FlinkTest {
+
+    @Test
+    void dataSetTest() throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSource<String> dataSource = env.readTextFile("hello.txt");
+
+        FlatMapOperator<String, Tuple2<String, Integer>> flatMapOperator = dataSource.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                for (String word : value.split(" ")) {
+                    Tuple2<String, Integer> tuple2 = Tuple2.of(word, 1);
+                    collector.collect(tuple2);
+                }
+            }
+        });
+
+        //group by index
+        UnsortedGrouping<Tuple2<String, Integer>> tuple2UnsortedGrouping = flatMapOperator.groupBy(0);
+
+        //sum by index
+        AggregateOperator<Tuple2<String, Integer>> result = tuple2UnsortedGrouping.sum(1);
+
+        result.print();
+        /**
+         * (flink,1)
+         * (world,1)
+         * (hello,3)
+         * (java,1)
+         */
+
+
+    }
+}
